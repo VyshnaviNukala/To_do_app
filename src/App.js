530822b2d026c355afe38872adb915f21e158120
VyshnaviNukala <84068358+VyshnaviNukala@users.noreply.github.com>
2023-12-28 00:33:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MainComponent from './mainComponent';
+function App() {
+  return (
+    <div>
+<MainComponent/>
+    </div>
+  );
+}
+
+export default App;
