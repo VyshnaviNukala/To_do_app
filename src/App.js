ae920771562c77199b4a4c85cb4a965ca9bed910
VyshnaviNukala <viashanvi.nukala26@gmail.com>
2023-12-30 00:35:07 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MainComponent from './mainComponent';
-import SideComponent from './SideComponent';
+
 function App() {
   return (
     <div>
-      <SideComponent/> 
+      
  <MainComponent/>
     </div>
   );
